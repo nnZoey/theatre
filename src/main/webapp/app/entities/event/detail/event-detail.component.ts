f9ed3c68f3ee7e9ef909fa650466efb73d0f3595
nnZoey <nguyentramzoey@gmail.com>
2023-01-08 15:32:09 +0700

    Book feature wip

--- src/main/webapp/app/entities/event/detail/event-detail.component.ts
+++ src/main/webapp/app/entities/event/detail/event-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ISeat } from 'app/entities/seat/seat.model';
+import { SeatService } from 'app/entities/seat/service/seat.service';
 
 import { IEvent } from '../event.model';
 
@@ -9,16 +11,102 @@ import { IEvent } from '../event.model';
 })
 export class EventDetailComponent implements OnInit {
   event: IEvent | null = null;
+  seats: ISeat[] = [];
+  alignedSeats: ISeat[][] = [];
+  selectedSeats: ISeat[] = [];
+  occupiedSeats: ISeat[] = [];
+  paying = false;
 
-  constructor(protected activatedRoute: ActivatedRoute) {}
+  constructor(protected activatedRoute: ActivatedRoute, protected seatService: SeatService) {}
 
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(({ event }) => {
       this.event = event;
+
+      const stageId = event.stage?.id;
+      if (stageId !== undefined) {
+        this.seatService.query({ 'stageId.equals': stageId }).subscribe({
+          next: res => {
+            this.seats = res.body ?? [];
+            this.occupiedSeats = this.seats.filter(s => s.occupied);
+            while (this.seats.length > 0) {
+              const seat = this.seats[0];
+              const alignedSeats = this.seats.filter(s => s.row === seat.row);
+              this.alignedSeats.push(alignedSeats);
+              this.seats = this.seats.filter(s => s.row !== seat.row);
+            }
+          },
+        });
+      }
     });
+
+    // generate seats row is char, column is number
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        const seat: ISeat = {
+          id: i * 10 + j,
+          row: String.fromCharCode(65 + i),
+          col: j,
+        };
+        this.seats.push(seat);
+      }
+    }
+
+    for (let i = 0; i < 10; i++) {
+      const seat = this.seats[Math.floor(Math.random() * this.seats.length)];
+      this.occupiedSeats.push(seat);
+    }
+
+    while (this.seats.length > 0) {
+      const seat = this.seats[0];
+      const alignedSeats = this.seats.filter(s => s.row === seat.row);
+      this.alignedSeats.push(alignedSeats);
+      this.seats = this.seats.filter(s => s.row !== seat.row);
+    }
   }
 
   previousState(): void {
     window.history.back();
+  }
+
+  selectSeat(seat: ISeat): void {
+    if (this.selectedSeats.find(s => s.id === seat.id) !== undefined) {
+      this.selectedSeats = this.selectedSeats.filter(s => s.id !== seat.id);
+    } else {
+      this.selectedSeats.push(seat);
+    }
+  }
+
+  isSelected(seat: ISeat): boolean {
+    return this.selectedSeats.find(s => s.id === seat.id) !== undefined;
+  }
+
+  isOccupied(seat: ISeat): boolean {
+    return this.occupiedSeats.find(s => s.id === seat.id) !== undefined;
+  }
+
+  getSeatClass(seat: ISeat): string {
+    if (this.isOccupied(seat)) {
+      return 'btn-outline-danger';
+    }
+    if (this.isSelected(seat)) {
+      return 'btn-success';
+    }
+    return 'btn-outline-primary';
+  }
+
+  getSelectedSeats(): string {
+    return this.selectedSeats
+      .map(seat => String(seat.row) + String(seat.col))
+      .sort()
+      .join(', ');
+  }
+
+  pay(): void {
+    this.paying = true;
+  }
+
+  cancelPay(): void {
+    this.paying = false;
   }
 }
